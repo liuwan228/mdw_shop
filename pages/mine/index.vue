d2443d1b80aa8678e2e6368530acaa785bce85c1
刘婉 <422242670@qq.com>
2023-03-01 16:12:55 +0800

    2

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -1,5 +1,12 @@
 <template>
-	<view class="mine">我的页面</view>
+	<view class="mine">
+		<view class="aaa">
+			1111我的页面
+		</view>
+		<view class="bbb">
+			222我的页面
+		</view>
+		</view>
 </template>
 
 <script>
@@ -7,6 +14,12 @@
 
 <style lang="scss" scoped>
 	.mine{
-		font-size: 38px;
+		font-size: 38rpx;
+		.aaa{
+			height: 100rpx;
+		}
+		.bbb{
+			height: 200rpx;
+		}
 	}
 </style>
